7c5ff3a43fdab06457b1e5abc52fc31c5521bd3b
Hao Yuan Zheng <xpoi5020@gmail.com>
2024-04-18 18:22:55 +0800

    Hello

---
+++ stdio.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#define ____ typedef
+#define long struct
+#define _____ int
+#define _______ void
+#define cin std::cout
+#define byte std::string
+#define unsigned std::endl
+#define do if
+#define new for
+#define switch while
+#define goto return
+#define catch else
+#define this sizeof
+#define class char
